b718ba4beafc1663da14f64814628a7166ee1612
Axel <axelrun@gmail.com>
2026-03-15 14:58:00 -0400

    Initial Commit

---
+++ MoviesIMiss2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoviesIMiss2
+//
+//  Created by Axel Christensen on 3/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviesIMiss2/MoviesIMiss2App.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesIMiss2App.swift
+//  MoviesIMiss2
+//
+//  Created by Axel Christensen on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct MoviesIMiss2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
